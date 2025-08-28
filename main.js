1f558307cf103ca0066efa6e567290d38f9408a8
Serg <kingkakoma9@gmail.com>
2025-08-28 16:04:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("File loaded");
+function showPanel(panelId) {
+  // Hide all panels
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => panel.classList.add('hidden'));
+
+  // Show selected one
+  document.getElementById(panelId).classList.remove('hidden');
+}
