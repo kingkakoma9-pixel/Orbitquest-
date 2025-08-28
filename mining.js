1e972925cd878ddb1ce6ab1605225bc931528680
Serg <kingkakoma9@gmail.com>
2025-08-28 18:38:37 +0200

    Delete mining.js

--- mining.js
+++
@@ -1,8 +0,0 @@
-let points = 0;
-const pointsDisplay = document.getElementById('points');
-const launchBtn = document.getElementById('launch-rocket');
-
-launchBtn.addEventListener('click', () => {
-    points += 10;
-    pointsDisplay.textContent = points;
-});
