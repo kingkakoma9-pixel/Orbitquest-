9eb22dab220da404d632cbe57085d848ec4a3eb4
Serg <kingkakoma9@gmail.com>
2025-08-28 16:09:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-function showPanel(panelId) {
+function showPanel(panelId, button) {
   // Hide all panels
   const panels = document.querySelectorAll('.panel');
   panels.forEach(panel => panel.classList.add('hidden'));
 
-  // Show selected one
+  // Show selected panel
   document.getElementById(panelId).classList.remove('hidden');
+
+  // Reset active state
+  const buttons = document.querySelectorAll('.navbar button');
+  buttons.forEach(btn => btn.classList.remove('active'));
+
+  // Highlight clicked button
+  button.classList.add('active');
 }
+
